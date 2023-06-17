41593412da6ce25ece5f7798955bf0095fb65712
Jogenius22 <108581854+Jogenius22@users.noreply.github.com>
2023-06-17 16:10:21 +0100

    Update widgetloader.js

--- widgetloader.js
+++ widgetloader.js
@@ -2,13 +2,10 @@
   // Create a container for the chat widget
   const container = document.createElement('div');
   container.id = 'my-chat-widget-container';
-  container.style.width = '350px';
-  container.style.height = '500px';
   container.style.position = 'fixed';
   container.style.bottom = '20px';
   container.style.right = '20px';
   container.style.zIndex = '1000';  // Ensure the chat widget appears above all other page elements
-  container.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.25)';  // Give the chat widget a drop shadow for a "raised" effect
   document.body.appendChild(container);
 
   // Create the chat icon
@@ -25,23 +22,20 @@
 
   // Create the iframe that will contain the chat widget, but keep it hidden initially
   const iframe = document.createElement('iframe');
-
   iframe.src = 'https://beyin-rtb7-gcdw5jx0h-jogenius22.vercel.app/chatwidget';  // replace with the URL of your deployed Next.js app
-   iframe.style.width = '100%';
-  iframe.style.height = '100%';
+   iframe.style.width = '400px';
+  iframe.style.height = '550px';
   iframe.style.border = 'none';
   iframe.style.display = 'none';  // keep the iframe hidden initially
+  iframe.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.25)';  // Give the chat widget a drop shadow for a "raised" effect
   container.appendChild(iframe);
 
   // Create the "close" button
   const closeButton = document.createElement('button');
   closeButton.textContent = 'X';
   closeButton.style.position = 'absolute';
-  closeButton.style.top = '-10px';
-  closeButton.style.right = '-10px';
-  closeButton.style.width = '20px';
-  closeButton.style.height = '20px';
-  closeButton.style.borderRadius = '50%';
+  closeButton.style.top = '16px';
+  closeButton.style.right = '16px';
   closeButton.style.backgroundColor = 'white';
   closeButton.style.border = '1px solid black';
   closeButton.style.display = 'none';  // keep the close button hidden initially
@@ -61,4 +55,3 @@
     closeButton.style.display = 'none';
   });
 })();
-
